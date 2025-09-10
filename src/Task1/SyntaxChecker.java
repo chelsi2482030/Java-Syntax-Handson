d357d5b02cb4bc084651c150cc29584eaf229f12
chelsi2482030 <2482030@unai.edu>
2025-09-10 11:59:25 +0700

    Update SyntaxChecker.java

--- src/Task1/SyntaxChecker.java
+++ src/Task1/SyntaxChecker.java
@@ -1,4 +1,13 @@
 package Task1;
 
-public class SyntaxChecker {
+class syntaxchecker {
+    public static void main(String[] args) {
+        // Coba identifikasi kesalahan dalam kode berikut
+        int number2  = 10;
+        String firstName = "Budi";
+        int classNumber = 25;
+        System.out.println("Hello World");
+        boolean isActive = true;
+        double nilaiRata = 85.5;
+    }
 }
