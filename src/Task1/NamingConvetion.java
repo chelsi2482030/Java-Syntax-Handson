0bdb297d14255c1e8e53cf70178195fe64e61a68
chelsi2482030 <2482030@unai.edu>
2025-09-10 11:59:48 +0700

    Update NamingConvetion.java

--- src/Task1/NamingConvetion.java
+++ src/Task1/NamingConvetion.java
@@ -1,13 +1,20 @@
 package Task1;
 
-class syntaxchecker {
+class NamingConvention {
     public static void main(String[] args) {
-        // Coba identifikasi kesalahan dalam kode berikut
-        int number2  = 10;
-        String firstName = "Budi";
-        int classNumber = 25;
-        System.out.println("Hello World");
-        boolean isActive = true;
-        double nilaiRata = 85.5;
+        String namaLengkapMahasiswa="Chelsi Enatalia N Parhusip";
+        String nimMahasiswa="2482030";
+        double ipkMahasiswa=3.82;
+        boolean statusAktifKuliah=true;
+        String alamatMahasiswa="permata regency,bumiwangi ciparay";
+        int jumlahSks=22;
+
+        //tampilkan semua data
+        System.out.println("===DATA MAHASISWA");
+        System.out.println("Nama Lengkap :" + namaLengkapMahasiswa);
+        System.out.println("NIM" + nimMahasiswa);
+        System.out.println("IPK" + ipkMahasiswa);
+        System.out.println("Status Kuliah :" + (statusAktifKuliah? "Aktif" : "Tidak Aktif"));
+        System.out.println("Jumlah SKS : " + jumlahSks);
     }
 }
